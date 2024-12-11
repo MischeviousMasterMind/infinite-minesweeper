289e303fc5bbc7c9796442bc55bf005946462c0a
1904557 <1904557@01-18111.pusd.dom>
2024-12-11 13:06:44 -0800

    Test

---
+++ src/minesweeper/Frame.java
@@ -0,0 +1,93 @@
+package minesweeper;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.ArrayList;
+import java.util.Comparator;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Frame extends JPanel implements ActionListener, MouseListener{
+	
+	final static int WIDTH = 1000, HEIGHT = 1000;
+	
+	public Frame() {
+		
+		JFrame window = new JFrame("Minesweeper");
+		
+		window.setSize(new Dimension(WIDTH, HEIGHT));
+		window.setBackground(Color.white);
+		window.add(this);
+		window.setResizable(false);
+		window.addMouseListener(this);
+		
+		// set custom cursor eventually
+		
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setVisible(true);
+		
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Frame frame = new Frame();
+		
+		ArrayList<Integer> list = new ArrayList(5);
+		
+		list.add(1);
+		list.add(2);
+		list.add(3);
+		list.add(4);
+		
+		list.forEach((n) -> {System.out.println(n);});
+		
+		Comparator<Integer> compare = (a, b) -> {return b - a;};
+		
+		list.sort(compare);
+		
+		list.forEach((n) -> {System.out.println(n);});
+		
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/minesweeper/Game.java
@@ -0,0 +1,22 @@
+package minesweeper;
+
+public class Game {
+	
+	private boolean[][] mineLayout = new boolean[100][100];
+	private int numberOfMines, numberOfFlags;
+	
+	public Game(int numberOfMines) {
+		
+		this.numberOfMines = numberOfMines;
+		
+		
+		
+	}
+	
+	public void generate() {
+		
+		
+		
+	}
+	
+}
