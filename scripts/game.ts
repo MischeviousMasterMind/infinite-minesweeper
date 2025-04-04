a72d886b6d0d397f593b7bd55b79ce4147ab7af5
MindFlares <73049474+MischeviousMasterMind@users.noreply.github.com>
2025-04-04 11:27:22 -0700

    Started writing Game class

--- scripts/game.ts
+++ scripts/game.ts
@@ -1,24 +1,51 @@
+
+class Game {
+
+    readonly flags: Field;
+    readonly sweeped: Field;
+
+    numberedTiles: number[][];
+    
+    readonly mines: Field;
+
+    constructor(mines: Field) {
+
+        this.mines = mines;
+
+    }
+
+}
+
 /**
- * A n-by-n minesweeper field that contains mines. Generate one using the #Field()
+ * A m-by-n minesweeper field that can contain mines, flags, or sweeped tiles.
+ * Values are stored in a 1-dimensional boolean array, but are treated as a rectangular array.
  * @author MindFlares
  */
 class Field {
 
     readonly width: number;
     readonly height: number;
-    readonly mines: Boolean[];
+    readonly numOfElements: number;
+    #field: Boolean[];
 
     /**
-     * Generates an minefield using the seed parameter with the dimensiosn specified by the width and height.
+     * Generates a field using the seed parameter with the dimensions specified by the width and height.
+     * 
+     * 
+     * 
      * @param width the width of the minefield
      * @param height the height of the minefield
      * @param seed the seed used to generate the minefield
      */
-    constructor(width: number, height: number, numOfMines = 0, seed?: bigint) {
+    constructor(width: number, height: number, numOfElements = 0, seed?: bigint) {
 
-        this.mines = new Boolean[width * height];
+        this.#field = new Boolean[width * height];
+        this.numOfElements = numOfElements;
 
-        if (seed === undefined) return;
+        if (numOfElements === 0) return;
+
+        if (seed === undefined)
+            seed = Field.generateRandomSeed(width * height, numOfElements);
 
         let subSeed = seed;
         let count = 0;
@@ -27,7 +54,7 @@ class Field {
 
             for (let ii = 0; ii < height; ii++) {
 
-                let diff = this.#nCr(width * height - i * height - ii - 1, numOfMines - 1 - count);
+                let diff = Field.#nCr(width * height - i * height - ii - 1, numOfElements - 1 - count);
 
                 if (subSeed >= seed && subSeed >= 0n) {
 
@@ -35,20 +62,51 @@ class Field {
 
                 } else {
 
-                    this.mines[i * width + ii] = true;
+                    this.#field[i * width + ii] = true;
                     count++;
 
-                    if (count === numOfMines)
+                    if (count === numOfElements)
                         return;
                 }
-
             }
-
         }
-
     }
 
-    #nCr(numOfElements: number, sizeOfSet: number) {  
+    /**
+     * Generates a random seed for the field of the specified size and number of elements.
+     * @param size the total area of the field (width multiplied by height)
+     * @param numOfElments the number of elements in the field
+     * @returns random seeed with the specified size and number of elements
+     */
+    static generateRandomSeed(size: number, numOfElements: number) {
+
+        return BigInt(Math.random()) * Field.#nCr(size, numOfElements);
+    }
+
+    /**
+     * Returns the value of the cell at the specified row and column of the field
+     * @param row the row in the field
+     * @param col the column in the field
+     * @returns values inside of the cell at the specified row and column
+     */
+    get(row: number, col: number) {
+
+        return this.#field[row * this.width + col];
+    }
+
+    /**
+     * Sets the value of the cell at the specified row and column of the field to the value specified
+     * @param row the row in the field
+     * @param col the column in the field
+     * @param value the value to set at the cell
+     */
+    set(row: number, col: number, value: boolean) {
+
+        this.#field[row * this.width + col] = value;
+    }
+
+    static #nCr(numOfElements: number, sizeOfSet: number) {
+
         let numOfCombinations: bigint = 1n;
 
         for (let i = 1; i <= numOfElements; i++)
