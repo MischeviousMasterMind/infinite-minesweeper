0cb5de912e8efe4d48e792f17487e519a09a46df
MindFlares <73049474+MischeviousMasterMind@users.noreply.github.com>
2025-04-04 10:33:40 -0700

    Finished seed generation

--- scripts/game.ts
+++ scripts/game.ts
@@ -6,14 +6,7 @@ class Field {
 
     readonly width: number;
     readonly height: number;
-    #mines: Boolean[];
-
-    /**
-     * Generates an empty minefield with the dimensions specified by the width and height.
-     * @param width the width of the minefield
-     * @param height the height of the minefield
-     */
-    constructor(width: number, height: number);
+    readonly mines: Boolean[];
 
     /**
      * Generates an minefield using the seed parameter with the dimensiosn specified by the width and height.
@@ -21,11 +14,11 @@ class Field {
      * @param height the height of the minefield
      * @param seed the seed used to generate the minefield
      */
-    constructor(width: number, height: number, seed?: bigint) {
+    constructor(width: number, height: number, numOfMines = 0, seed?: bigint) {
 
-        this.#mines = new Boolean[width * height];
+        this.mines = new Boolean[width * height];
 
-        if (seed == undefined) return;
+        if (seed === undefined) return;
 
         let subSeed = seed;
         let count = 0;
@@ -34,33 +27,38 @@ class Field {
 
             for (let ii = 0; ii < height; ii++) {
 
-                if(subSeed >= seed && subSeed !== BigInt(0)) {
+                let diff = this.#nCr(width * height - i * height - ii - 1, numOfMines - 1 - count);
 
-                    subSeed = subSeed.substract(this.#nCr(0, 0, 0));
+                if (subSeed >= seed && subSeed >= 0n) {
+
+                    subSeed -= diff;
 
                 } else {
 
+                    this.mines[i * width + ii] = true;
+                    count++;
+
+                    if (count === numOfMines)
+                        return;
                 }
 
             }
-
 
         }
 
     }
 
-    #nCr(numOfElements: number, sizeOfSet: number) {
-
-        let numOfCombinations = 1;
+    #nCr(numOfElements: number, sizeOfSet: number) {  
+        let numOfCombinations: bigint = 1n;
 
         for (let i = 1; i <= numOfElements; i++)
-            numOfCombinations *= i;
+            numOfCombinations *= BigInt(i);
 
         for (let i = 1; i <= sizeOfSet; i++)
-            numOfCombinations /= i;
+            numOfCombinations /= BigInt(i);
 
         for (let i = 1; i <= numOfElements - sizeOfSet; i++)
-            numOfCombinations /= i;
+            numOfCombinations /= BigInt(i);
 
         return numOfCombinations;
     }
